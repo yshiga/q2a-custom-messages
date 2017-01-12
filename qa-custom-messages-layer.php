fb094c243a7c40cee927bdd87dcc529b7d86caff
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-01-12 11:57:37 +0900

    main でなく main_part を使う

--- qa-custom-messages-layer.php
+++ qa-custom-messages-layer.php
@@ -1,19 +1,28 @@
 <?php
 class qa_html_theme_layer extends qa_html_theme_base {
-  public function main() {
+  public function main_parts($content) {
     if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'messages') {
       // $template = file_get_contents(CML_DIR . '/messages-template.html');
       // $this->output($template);
-      $messages = $this->content['message_list']['messages'];
+      $messages = $content['message_list']['messages'];
       $path = CML_DIR . '/messages-template.html';
       include $path;
     // } else if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'message') {
     //   $template = file_get_contents(CML_DIR . '/message-template.html');
     //   $this->output($template);
     } else {
-      qa_html_theme_base::main();
+      qa_html_theme_base::main_parts($content);
     }
   }
+  
+  public function page_title_error() {
+    if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'messages') {
+    }else {
+      qa_html_theme_base::page_title_error();
+    }
+    
+  }
+  
   public function nav($navtype, $level=null)
 	{
     if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'messages') {
