766b919277437b7b78c85daf7872fdd3ffe407ce
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-01-12 11:18:36 +0900

    一旦 message は元の表示にする

--- qa-custom-messages-layer.php
+++ qa-custom-messages-layer.php
@@ -4,21 +4,12 @@ class qa_html_theme_layer extends qa_html_theme_base {
     if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'messages') {
       // $template = file_get_contents(CML_DIR . '/messages-template.html');
       // $this->output($template);
-      $messages = array();
-      // print_r($this->content['message_list']['messages']);
-      foreach ($this->content['message_list']['messages'] as $message) {
-        $temp = array();
-        $temp['content'] = $message['content'];
-        $temp['avatarblobid'] = $message['raw']['fromavatarblobid'];
-        $temp['messageurl'] = '/message/'.$message['raw']['fromhandle'];
-        $temp['handle'] = $message['raw']['fromhandle'];
-        $messages[] = $temp;
-      }
+      $messages = $this->content['message_list']['messages'];
       $path = CML_DIR . '/messages-template.html';
       include $path;
-    } else if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'message') {
-      $template = file_get_contents(CML_DIR . '/message-template.html');
-      $this->output($template);
+    // } else if (qa_opt('site_theme') === CML_TARGET_THEME_NAME && $this->template === 'message') {
+    //   $template = file_get_contents(CML_DIR . '/message-template.html');
+    //   $this->output($template);
     } else {
       qa_html_theme_base::main();
     }
